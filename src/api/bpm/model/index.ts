fe744d5e58f3fb924259a8627a9edd6e01fe62a6
miazzy <qq695926920@163.com>
2023-09-22 11:14:40 +0800

    调整,#12680,平台框架：功能权限、功能菜单、登录前后端联调

--- src/api/bpm/model/index.ts
+++ src/api/bpm/model/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defHttp } from '/@/utils/http/axios';
 
 export type ProcessDefinitionVO = {
@@ -73,68 +72,3 @@ export const deployModel = async (id: number) => {
   const requestParams = { url: BpmModelApi.DeployModel + id };
   return defHttp.post<any>(requestParams, {});
 };
-=======
-// import request from '@/config/axios'
-
-export type ProcessDefinitionVO = {
-  id: string
-  version: number
-  deploymentTIme: string
-  suspensionState: number
-}
-
-// √
-export type ModelVO = {
-  id: number
-  formName: string
-  key: string
-  name: string
-  description: string
-  category: string
-  formType: number
-  formId: number
-  formCustomCreatePath: string
-  formCustomViewPath: string
-  processDefinition: ProcessDefinitionVO
-  status: number
-  remark: string
-  createTime: string
-  bpmnXml: string
-}
-
-// export const getModelPage = async (params) => {
-//   return await request.get({ url: '/bpm/model/page', params })
-// }
-
-// √
-export const getModel = async (id: number) => {
-  // TODO url: '/bpm/model/get?id=' + id
-}
-
-// √
-export const updateModel = async (data: ModelVO) => {
-  // TODO url: '/bpm/model/update'
-}
-
-// 任务状态修改
-// export const updateModelState = async (id: number, state: number) => {
-//   const data = {
-//     id: id,
-//     state: state
-//   }
-//   return await request.put({ url: '/bpm/model/update-state', data: data })
-// }
-
-// √
-export const createModel = async (data: ModelVO) => {
-  // TODO url: '/bpm/model/create'
-}
-
-// export const deleteModel = async (id: number) => {
-//   return await request.delete({ url: '/bpm/model/delete?id=' + id })
-// }
-
-// export const deployModel = async (id: number) => {
-//   return await request.post({ url: '/bpm/model/deploy?id=' + id })
-// }
->>>>>>> 006f636e5f892115d812bc0c092617635f61184b
