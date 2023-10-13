dab3f2c19410e7d26bd9c0fb5468e7298e6a74b3
Mr.Yuan <15208170620m@sina.cn>
2023-10-13 11:05:49 +0800

    增加配置；

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -185,11 +185,11 @@ const transform: AxiosTransform = {
         ? `${options.authenticationScheme} ${token}`
         : token;
       // TODO 代理设置
-      if (config.url?.startsWith('/bpm')) {
-        (config as Recordable).headers.Authorization = 'Bearer test1';
-        (config as Recordable).headers['Tenant-Id'] = 1;
-        (config as Recordable).headers['login_user_type'] = 2;
-      }
+      // if (config.url?.startsWith('/admin-api/bpm')) {
+      //   (config as Recordable).headers.Authorization = 'Bearer test1';
+      //   (config as Recordable).headers['Tenant-Id'] = 1;
+      //   (config as Recordable).headers['login_user_type'] = 2;
+      // }
     }
     return config;
   },
