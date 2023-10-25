4989e304381a3f5a73d11aeb4efdc71a73a2b827
miazzy <qq695926920@163.com>
2023-10-25 11:12:56 +0800

    调整,#12680,修复刷新报错

--- src/settings/projectSetting.ts
+++ src/settings/projectSetting.ts
@@ -131,7 +131,7 @@ const setting: ProjectConfig = {
     // Whether to show the refresh button
     showRedo: true,
     // Whether to show the collapse button
-    showFold: true,
+    showFold: false, // 隐藏Tab页面折叠栏
   },
 
   // Transition Setting
