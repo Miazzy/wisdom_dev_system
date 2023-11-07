1e19fed932043c707cf6b30ad75eba0e92632735
miazzy <qq695926920@163.com>
2023-11-07 09:56:27 +0800

    调整,#12824,安全大屏/综合大屏调整

--- src/views/da/cockpit/composition/RightContent.vue
+++ src/views/da/cockpit/composition/RightContent.vue
@@ -19,9 +19,21 @@
         :data="securityData"
         style="width: 80%; margin-left: 10%"
       />
-      <div style="width: 100%; height: 185px">
+      <!-- <div style="width: 100%; height: 185px">
         <div style="margin: -40px 0 0 20px; transform: scale(0.85) translate(-60px, 0px)">
           <EchartPillarChart :data="pchartData" :width="620" :height="260" />
+        </div>
+      </div> -->
+      <div style="width: 100%; height: 185px;">
+        <div style="margin: -40px 0 0 -15px; transform: scale(0.85) translate(-60px, 0px)">
+          <EchartLineBarChart
+            :data="lbarChartData"
+            :colors="chartColors"
+            :width="680"
+            :height="310"
+            :category="pchartData.mName"
+            ybgcolor="#01B7D730"
+          />
         </div>
       </div>
     </div>
@@ -54,12 +66,11 @@
   import EchartPillarChart from '/@/components/Framework/Chart/EchartPillarChart.vue';
   import DictSelectBox from '@/components/Framework/Combox/DictSelectBox.vue';
   import IndicatorGroup from '/@/components/Framework/Chart/IndicatorGroup.vue';
+  import EchartLineBarChart from '/@/components/Framework/Chart/EchartLineBarChart.vue';
   import { DICT_TYPE } from '@/utils/dict';
 
   const pchartData = ref({
-    lData: [25, 28, 39, 14, 22, 14, 33, 39, 14, 22, 14, 33],
     mData: [15, 22, 39, 14, 31, 15, 20, 39, 14, 31, 15, 20],
-    sData: [13, 20, 10, 34, 15, 30, 11, 10, 34, 15, 30, 11],
     mName: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
     units: ['单位', '单位'],
   });
@@ -97,6 +108,15 @@
     { label: '40岁至50岁', value: 33 },
     { label: '50岁至60岁', value: 55 },
     { label: '60岁至100岁', value: 15 },
+  ];
+
+  const chartColors = ref([['#1C5C99', '#4CAFF9'], ['#105D3A', '#55CFD0'], '#f39c12', '#d81b60']);
+
+  const lbarChartData = [
+    [50.0, 34.9, 74.0, 44.2, 55.6, 76.7, 35.6, 62.2, 32.6, 20.0, 62.4, 32.3],
+    [32.6, 53.9, 39.0, 56.4, 48.7, 50.7, 75.6, 31.2, 48.7, 39.8, 62.0, 52.3],
+    [32.0, 39.2, 13.3, 24.5, 32.3, 15.2, 20.3, 23.4, 23.0, 16.5, 12.0, 26.2],
+    [12.0, 29.2, 33.3, 29.5, 35.3, 10.2, 28.3, 33.4, 13.0, 15.5, 14.0, 26.2],
   ];
 </script>
 
