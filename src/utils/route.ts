96824f0756f25a2db7b00f6fde9b8361de0d1061
miazzy <qq695926920@163.com>
2023-11-27 15:27:01 +0800

    调整,#13078,【前端框架】功能跳转URL(path)，携带参数，页面接收方法提供

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -1,14 +1,10 @@
 export const parseRoutePath = (path: string): Record<string, string> => {
   const segments = path.split('/');
   const lastSegment = segments[segments.length - 1];
-  // 替换 __ 为 =
-  const replacedEquals = lastSegment.replace(/__/g, '=');
-  // 替换 !! 为 &
-  const replacedAmpersand = replacedEquals.replace(/!!/g, '&');
-  // 替换 _aa_ 为 &
-  const replacedUnderscore = replacedAmpersand.replace(/_aa_/g, '&');
-  // 解析为对象
-  const queryParams: Record<string, string> = {};
+  const replacedEquals = lastSegment.replace(/__/g, '='); // 替换 __ 为 =
+  const replacedAmpersand = replacedEquals.replace(/!!/g, '&'); // 替换 !! 为 &
+  const replacedUnderscore = replacedAmpersand.replace(/_aa_/g, '&'); // 替换 _aa_ 为 &
+  const queryParams: Record<string, string> = {}; // 解析为对象
   const pairs = replacedUnderscore.split('&');
   for (const pair of pairs) {
     const [key, value] = pair.split('=');
