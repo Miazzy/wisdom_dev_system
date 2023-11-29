b3877c35881d665a8ec0ef7e8db5de56edc12a91
zhangzr <zhangzr03@tongwei.com>
2023-11-29 16:15:33 +0800

    基础设置-公共API

--- src/api/baseset/common/index.ts
+++ src/api/baseset/common/index.ts
@@ -4,6 +4,8 @@ import { defHttp } from '/@/utils/http/axios';
 export enum CommonApi {
   GetOrgStationPeriodTree = '/baseset/common/list-orgStationPeriodTree',
   GetOrgStationWarehouseTree = '/baseset/common/list-stationWarehouse',
+  LIST_STATION_TREE = '/baseset/common/list-orgStationTree',
+  LIST_STATION_PERSON = '/baseset/common/list-stationPerson',
 }
 
 export const getOrgStationPeriodTree = async (params) => {
@@ -15,3 +17,23 @@ export const getOrgStationWarehouseTree = async (params) => {
   const requestParams = { url: CommonApi.GetOrgStationWarehouseTree, params };
   return defHttp.post<any>(requestParams, { isOnlyResult: true });
 };
+
+// 获取电站树
+export const getStationTree = (params) =>
+  defHttp.post(
+    {
+      url: CommonApi.LIST_STATION_TREE,
+      params,
+    },
+    {},
+  );
+
+// 获取电站下的组织人员
+export const getStationPerson = (params) =>
+  defHttp.post(
+    {
+      url: CommonApi.LIST_STATION_PERSON,
+      params,
+    },
+    {},
+  );
