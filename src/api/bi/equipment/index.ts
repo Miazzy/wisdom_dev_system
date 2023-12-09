e11d7fe35f54dbd12b43c74101a4a3357285fdf3
Mr.Yuan <15208170620m@sina.cn>
2023-12-09 17:10:22 +0800

    新增BI报表接口：库存结存；

--- src/api/bi/equipment/index.ts
+++ src/api/bi/equipment/index.ts
@@ -2,17 +2,23 @@ import { defHttp } from '@/utils/http/axios';
 
 // 查询设备入库结存列表
 export function getEquipmentBalancePage(params) {
-  return defHttp.get({ url: '/bi/equipment-balance/page', params });
+  return defHttp.get({ url: '/bi/equipment-balance/page', params }, { isOnlyResult: true });
 }
 
 // 查询设备入库明细列表
 export function getEquipmentWarehousingDtlPage(params) {
-  return defHttp.get({ url: '/bi/equipment-balance/getEquipmentWarehousingDtlPage', params });
+  return defHttp.get(
+    { url: '/bi/equipment-balance/getEquipmentWarehousingDtlPage', params },
+    { isOnlyResult: true },
+  );
 }
 
 // 查询设备出库明细列表
 export function getEquipmentOutboundDtlPage(params) {
-  return defHttp.get({ url: '/bi/equipment-balance/getEquipmentOutboundDtlPage', params });
+  return defHttp.get(
+    { url: '/bi/equipment-balance/getEquipmentOutboundDtlPage', params },
+    { isOnlyResult: true },
+  );
 }
 
 // 导出设备入库结存 Excel
