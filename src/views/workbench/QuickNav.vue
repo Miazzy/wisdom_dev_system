d30950d10de47828e2c7de89a7f1f6a337352073
lwr <610507101@qq.com>
2023-12-21 13:49:47 +0800

    功能开发#14027：工作台前端静态页面开发，深色和浅色。

---
+++ src/views/workbench/QuickNav.vue
@@ -0,0 +1,162 @@
+<template>
+  <a-card style="height: 326px" title="快捷导航" :bordered="false">
+    <template #extra>
+      <Icon icon="ion:settings-outline" :size="17" />
+    </template>
+    <div class="card-content">
+      <a-carousel :after-change="onChange" dotsClass="carousel-dots">
+        <div class="nav-page" v-for="(pageItem, pageIndex) in navList" :key="pageIndex">
+          <div class="nav-item-container">
+            <div class="nav-item" v-for="(n, index) in pageItem" :key="index">
+              <div class="nav-icon-box" :style="{backgroundColor: colors[index] }">
+                <Icon :icon="n.icon" :size="32" />
+              </div>
+              <div class="nav-label">{{n.label}}</div>
+            </div>
+          </div>
+        </div>
+      </a-carousel>
+    </div>
+  </a-card>
+</template>
+<script lang="ts" setup>
+  import Icon from '@/components/Icon/Icon.vue';
+  import { ref, reactive } from 'vue';
+
+  const colors = [
+    '#FBCC5A',
+    '#3FD3A6',
+    '#F28E27',
+    '#38BBFE',
+    '#4799F5',
+    '#F57C47',
+    '#6880FA',
+    '#58DD96',
+    '#4799F5',
+    '#379EFF',
+  ];
+  const navList = ref([
+    [
+      {
+        label: '电站运营',
+        icon: 'ion:tv',
+      },
+      {
+        label: '分区总览',
+        icon: 'ion:layers',
+      },
+      {
+        label: '组串总览',
+        icon: 'ion:easel',
+      },
+      {
+        label: '告警总览',
+        icon: 'ion:notifications',
+      },
+      {
+        label: '生产管理',
+        icon: 'ion:speedometer',
+      },
+      {
+        label: '电气一票',
+        icon: 'ion:scale-sharp',
+      },
+      {
+        label: '隐患管理',
+        icon: 'ion:warning',
+      },
+      {
+        label: '巡检执行',
+        icon: 'ion:map',
+      },
+      {
+        label: '缺陷管理',
+        icon: 'ion:build',
+      },
+      {
+        label: '工单管理',
+        icon: 'ion:document-attach',
+      },
+    ],
+    [
+      {
+        label: '物资台账',
+        icon: 'ion:receipt',
+      },
+      {
+        label: '计划电量统计',
+        icon: 'ion:bar-chart',
+      },
+      {
+        label: '隐患统计',
+        icon: 'ion:shield-checkmark',
+      },
+      {
+        label: '缺陷统计',
+        icon: 'ion:pie-chart',
+      },
+    ],
+  ]);
+
+  const onChange = (current: number) => {
+    console.log(current);
+  };
+</script>
+<style lang="less" scoped>
+  .card-content {
+    height: 278px;
+    .nav-page {
+      height: 278px;
+      padding: 16px 0;
+    }
+    .nav-item-container {
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+    }
+    .nav-item {
+      width: 20%;
+      height: 88px;
+      &:first-child {
+        margin-bottom: 45px;
+      }
+      .nav-icon-box {
+        width: 58px;
+        height: 58px;
+        border-radius: 16px;
+        text-align: center;
+        padding: 13px 0;
+        margin: 0 auto;
+        cursor: pointer;
+      }
+      .nav-label {
+        text-align: center;
+        font-size: 14px;
+        padding-top: 16px;
+        line-height: 1;
+        cursor: pointer;
+        width: 100%;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+    :deep(.carousel-dots) {
+      bottom: 0;
+    }
+    :deep(.carousel-dots > li) {
+      width: 32px;
+      height: 4px;
+      border-radius: 2px;
+      button {
+        background-color: transparent;
+      }
+      &.slick-active {
+        background-color: #1890ff;
+        button {
+          background-color: transparent;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/workbench/Workbench.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="workbench">
+    <a-row :gutter="[10,10]">
+      <a-col :span="24">
+        <!-- 头部 -->
+        <WorkbenchHeader />
+      </a-col>
+      <a-col :span="16">
+        <!-- 数据看板 -->
+        <DataBoard />
+      </a-col>
+      <a-col :span="8">
+        <!-- 快捷导航 -->
+        <QuickNav />
+      </a-col>
+      <a-col :span="16">
+        <!-- 流程审批 -->
+        <Approval />
+      </a-col>
+      <a-col :span="8">
+        <!-- 日程安排 -->
+        <Schedule />
+      </a-col>
+      <a-col :span="16">
+        <!-- 通知公告 -->
+        <Notice />
+      </a-col>
+      <a-col :span="8">
+        <!-- 工作提醒 -->
+        <WorkReminder />
+      </a-col>
+    </a-row>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { onMounted, reactive, ref, unref, UnwrapRef, toRaw } from 'vue';
+  import WorkbenchHeader from './WorkbenchHeader.vue';
+  import DataBoard from './DataBoard.vue';
+  import QuickNav from './QuickNav.vue';
+  import Approval from './Approval.vue';
+  import Schedule from './Schedule.vue';
+  import Notice from './Notice.vue';
+  import WorkReminder from './WorkReminder.vue';
+</script>
+<style lang="less" scoped>
+  .workbench {
+    width: 100%;
+    padding: 10px;
+    padding-right: 0;
+    & > :deep(.ant-row) {
+      width: 100%;
+    }
+    :deep(.ant-card-body) {
+      padding: 0 16px;
+    }
+    :deep(.ant-card-head) {
+      padding: 0 16px;
+      font-size: 14px;
+      font-weight: 600;
+    }
+    :deep(.ant-card-head-title) {
+      padding: 12px 0;
+    }
+    :deep(.ant-card-extra) {
+      padding: 12px 0;
+      &>span {
+        cursor: pointer;
+      }    
+    }
+  }
+</style>
