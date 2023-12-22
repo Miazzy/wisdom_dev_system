5f0b66b30054c8d1aff510ad2be188e2556af421
Mr.Yuan <15208170620m@sina.cn>
2023-12-22 17:26:13 +0800

    工作台接口联调；

--- src/views/workbench/Approval.vue
+++ src/views/workbench/Approval.vue
@@ -1,37 +1,34 @@
 <template>
   <a-card title="流程审批" :bordered="false">
     <template #extra>
-      <span>更多 &gt;</span>
+      <span @click="toMorePage">更多 &gt;</span>
     </template>
     <div class="card-content">
       <a-tabs v-model:activeKey="activeKey" @change="handleTabChange">
         <a-tab-pane v-for="(item, index) in tabList" :key="item.key">
           <template #tab>
-            <a-badge :count="item.count" :offset="[16,-12]" :numberStyle="{transform: 'scale(0.9)'}">
+            <a-badge
+              :count="item.count"
+              :offset="[16, -12]"
+              :numberStyle="{ transform: 'scale(0.9)' }"
+            >
               <span class="tab-text">
-                {{item.tab}}
+                {{ item.tab }}
               </span>
             </a-badge>
           </template>
           <div class="table-box">
-            <BasicTable @register="tableList[index]" :scroll="{y: 248}">
+            <BasicTable @register="tableList[index]" :scroll="{ y: 248 }">
               <template #bodyCell="{ column, record }">
                 <template v-if="column.dataIndex === 'action'">
-                  <TableAction :actions="[
-                          {
-                            label: '查看',
-                            onClick: handleCheck.bind(null, record),
-                          },
-                          {
-                            label: '审核',
-                            onClick: handleApprove.bind(null, record),
-                          },
-                          {
-                            label: '退回',
-                            color: 'error',
-                            onClick: handleBack.bind(null, record),
-                          },
-                        ]" />
+                  <TableAction
+                    :actions="[
+                      {
+                        label: '查看',
+                        onClick: handleCheck.bind(null, record),
+                      },
+                    ]"
+                  />
                 </template>
               </template>
             </BasicTable>
@@ -43,21 +40,29 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref, reactive } from 'vue';
-  import { BasicTable, useTable, TableAction, BasicColumn } from '/@/components/Table';
+  import { ref, onMounted } from 'vue';
   import { assign } from 'lodash-es';
+  import { useRouter } from 'vue-router';
+  import { BasicTable, useTable, TableAction, BasicColumn } from '/@/components/Table';
+  import * as TaskApi from '@/api/bpm/task';
+  import * as ProcessInstanceApi from '@/api/bpm/processInstance';
+  import { useUserStore } from '/@/store/modules/user';
+
+  const router = useRouter();
+  const userStore = useUserStore();
+  const getUserInfo = userStore.getUserInfo;
 
   // tab
-  const tabList = [
+  const tabList = ref([
     {
       key: '1',
       tab: '待处理',
-      count: 5,
+      count: 0,
     },
     {
       key: '2',
       tab: '待阅读',
-      count: 10,
+      count: 0,
     },
     {
       key: '3',
@@ -67,11 +72,33 @@
     {
       key: '4',
       tab: '已处理',
-      count: 99,
+      count: 0,
     },
-  ];
+  ]);
   const activeKey = ref('1');
-  const handleTabChange = (activeKey) => {};
+  const handleTabChange = (item) => {
+    if (item === '1') {
+      doTodo();
+    } else if (item === '2') {
+      doCC();
+    } else if (item === '3') {
+      doMy();
+    } else if (item === '4') {
+      doDone();
+    }
+  };
+
+  const toMorePage = () => {
+    if (activeKey.value == '1') {
+      router.push(`/bpm/task/todo`);
+    } else if (activeKey.value == '2') {
+      router.push(`/bpm/task/todo`);
+    } else if (activeKey.value == '3') {
+      router.push(`/bpm/task/processInstance`);
+    } else if (activeKey.value == '4') {
+      router.push(`/bpm/task/done`);
+    }
+  };
 
   // table
   const tableColumns: BasicColumn[] = [
@@ -121,42 +148,42 @@
       dataIndex: 'action',
     },
   };
-  const tableDataSource1 = [
-    {
-      title: '升压站楼梯有锈迹',
-      flowName: '待处理',
-      dept: '江苏如东电站',
-      person: '李木林',
-      time: '2023-06-28 10:44:06',
-    },
-  ];
-  const tableDataSource2 = [
-    {
-      title: '升压站楼梯有锈迹',
-      flowName: '待阅读',
-      dept: '江苏如东电站',
-      person: '李木林',
-      time: '2023-06-28 10:44:06',
-    },
-  ];
-  const tableDataSource3 = [
-    {
-      title: '升压站楼梯有锈迹',
-      flowName: '我发起',
-      dept: '江苏如东电站',
-      person: '李木林',
-      time: '2023-06-28 10:44:06',
-    },
-  ];
-  const tableDataSource4 = [
-    {
-      title: '升压站楼梯有锈迹',
-      flowName: '已处理',
-      dept: '江苏如东电站',
-      person: '李木林',
-      time: '2023-06-28 10:44:06',
-    },
-  ];
+  const tableDataSource1 = ref([
+    // {
+    //   title: '升压站楼梯有锈迹',
+    //   flowName: '待处理',
+    //   dept: '江苏如东电站',
+    //   person: '李木林',
+    //   time: '2023-06-28 10:44:06',
+    // },
+  ]);
+  const tableDataSource2 = ref([
+    // {
+    //   title: '升压站楼梯有锈迹',
+    //   flowName: '待阅读',
+    //   dept: '江苏如东电站',
+    //   person: '李木林',
+    //   time: '2023-06-28 10:44:06',
+    // },
+  ]);
+  const tableDataSource3 = ref([
+    // {
+    //   title: '升压站楼梯有锈迹',
+    //   flowName: '我发起',
+    //   dept: '江苏如东电站',
+    //   person: '李木林',
+    //   time: '2023-06-28 10:44:06',
+    // },
+  ]);
+  const tableDataSource4 = ref([
+    // {
+    //   title: '升压站楼梯有锈迹',
+    //   flowName: '已处理',
+    //   dept: '江苏如东电站',
+    //   person: '李木林',
+    //   time: '2023-06-28 10:44:06',
+    // },
+  ]);
   const [registerApprovalTable1] = useTable(
     assign(
       {
@@ -201,11 +228,90 @@
     registerApprovalTable4,
   ]);
   // 查看
-  const handleCheck = (record) => {};
-  // 审核
-  const handleApprove = (record) => {};
-  // 退回
-  const handleBack = (record) => {};
+  const handleCheck = (record) => {
+    router.push(`${record.viewPath}?processInstanceId=${record.processInstanceId}`);
+  };
+
+  const doTodo = async () => {
+    //待处理
+    const getTodoTask = await TaskApi.getTodoTaskPage();
+    tableDataSource1.value.splice(0, tableDataSource1.value.length);
+    if (getTodoTask) {
+      tabList.value[0].count = getTodoTask.total;
+      getTodoTask.list.forEach((element) => {
+        tableDataSource1.value.push({
+          title: element.name,
+          flowName: element.processInstance.name,
+          dept: element.processInstance.deptName,
+          person: element.processInstance.startUserNickname,
+          time: element.createTime,
+          viewPath: element.bpmProcessDefinitionRespVO.formCustomCreatePath,
+          processInstanceId: element.processInstance.id,
+        });
+      });
+    }
+  };
+
+  const doCC = async () => {
+    //待阅读
+    const getCCTaskPage = await TaskApi.getTodoCCPage();
+    tableDataSource2.value.splice(0, tableDataSource2.value.length);
+    if (getCCTaskPage) {
+      tabList.value[1].count = getCCTaskPage.total;
+      getCCTaskPage.list.forEach((element) => {
+        tableDataSource2.value.push({
+          title: element.name,
+          flowName: element.processInstance.name,
+          dept: element.processInstance.deptName,
+          person: element.processInstance.startUserNickname,
+          time: element.createTime,
+          viewPath: element.bpmProcessDefinitionRespVO.formCustomCreatePath,
+          processInstanceId: element.processInstance.id,
+        });
+      });
+    }
+  };
+
+  const doMy = async () => {
+    //我发起
+    const getMyProcessInstancePage = await ProcessInstanceApi.getMyProcessInstancePage();
+    if (getMyProcessInstancePage) {
+      getMyProcessInstancePage.list.forEach((element) => {
+        tableDataSource3.value.push({
+          title: element.name,
+          flowName: element.name,
+          dept: element.deptName,
+          person: getUserInfo.username,
+          time: element.createTime,
+          viewPath: element.bpmProcessDefinitionRespVO.formCustomCreatePath,
+          processInstanceId: element.id,
+        });
+      });
+    }
+  };
+
+  const doDone = async () => {
+    //已处理
+    const getDoneTaskPage = await TaskApi.getDoneTaskPage();
+    if (getDoneTaskPage) {
+      getDoneTaskPage.list.forEach((element) => {
+        tableDataSource4.value.push({
+          title: element.name,
+          flowName: element.processInstance.name,
+          dept: element.processInstance.deptName,
+          person: element.processInstance.startUserNickname,
+          time: element.createTime,
+          viewPath: element.bpmProcessDefinitionRespVO.formCustomCreatePath,
+          processInstanceId: element.processInstance.id,
+        });
+      });
+    }
+  };
+
+  /** 初始化 */
+  onMounted(async () => {
+    doTodo();
+  });
 </script>
 
 <style lang="less" scoped>
@@ -227,8 +333,7 @@
       }
     }
     :deep(.ant-table-thead th.text-center) {
-      
-        text-align: center !important;
+      text-align: center !important;
     }
     .table-box {
       height: 286px;
--- src/views/workbench/Notice.vue
+++ src/views/workbench/Notice.vue
@@ -1,16 +1,21 @@
 <template>
   <a-card title="通知公告" :bordered="false">
     <template #extra>
-      <span>更多 &gt;</span>
+      <span @click="toMorePage">更多 &gt;</span>
     </template>
     <div class="card-content">
       <ul class="notice-list-box">
-        <li class="notice-item" v-for="(item, index) in noticeList" :key="index">
+        <li
+          class="notice-item"
+          @click="toNotice(item)"
+          v-for="(item, index) in noticeList"
+          :key="index"
+        >
           <span class="item-dot"></span>
-          <span class="title-text" :class="{isRead: item.isRead}">{{item.title}}</span>
-          <span class="dept-text">{{item.dept}}</span>
-          <span class="person-text">{{item.person}}</span>
-          <span class="time-text">{{item.time}}</span>
+          <span class="title-text" :class="{ isRead: item.isRead }">{{ item.title }}</span>
+          <span class="dept-text">{{ item.dept }}</span>
+          <span class="person-text">{{ item.person }}</span>
+          <span class="time-text">{{ item.time }}</span>
         </li>
       </ul>
     </div>
@@ -18,60 +23,110 @@
 </template>
 
 <script lang="ts" setup>
-  import { ref, reactive } from 'vue';
+  import { ref, onMounted } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  import * as DateUtil from '/@/utils/dateUtil';
+  import * as WorkbenchApi from '@/api/workbench';
+
+  const router = useRouter();
+
+  const urls: any = ref([
+    { key: 'notice', url: '/oa/info/infonotice/create' },
+    { key: 'announcement', url: '/oa/info/officedoc/create' },
+  ]);
+
+  const toMorePage = () => {
+    router.push(`/oa/info/officedoc/index`);
+  };
+
+  const toNotice = (item) => {
+    const element = urls.value.find((e) => {
+      return e?.key === item.businessType;
+    });
+    if (element) {
+      router.push({ path: element.url, query: { id: item.id } });
+    }
+  };
 
   const noticeList = ref([
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-      isRead: 1
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
-    {
-      title:
-        '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
-      dept: '人行部',
-      person: '冯美清',
-      time: '2020-05-14 12:00',
-    },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    //   isRead: 1,
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
+    // {
+    //   title:
+    //     '物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知物联发〔2023〕27号关于更新运营中心2023年第三季度电站“安全主要负责人”“安全员”名单的通知',
+    //   dept: '人行部',
+    //   person: '冯美清',
+    //   time: '2020-05-14 12:00',
+    // },
   ]);
+
+  const getNoticeAndAnnouncementPage = async () => {
+    const noticeAndAnnouncement = await WorkbenchApi.getNoticeAndAnnouncementPage({
+      pageNo: 1,
+      pageSize: 10,
+    });
+    if (noticeAndAnnouncement) {
+      noticeList.value.splice(0, noticeList.value.length);
+      noticeAndAnnouncement.list.forEach((e) => {
+        noticeList.value.push({
+          id: e.id,
+          title: e.subject,
+          dept: e.deptName,
+          person: e.personMemberName,
+          time: DateUtil.formatToDate(e.finishedDate, 'YYYY-MM-DD HH:mm'),
+          isRead: e.isRead,
+          businessType: e.businessType,
+        });
+      });
+    }
+  };
+
+  /** 初始化 */
+  onMounted(async () => {
+    getNoticeAndAnnouncementPage();
+  });
 </script>
 
 <style lang="less" scoped>
@@ -85,26 +140,25 @@
       line-height: 42px;
       .item-dot {
         width: 5px;
-        height: 5px;  
+        height: 5px;
         border-radius: 50%;
       }
       .title-text {
         width: 76%;
-        opacity: 0.97;       
+        opacity: 0.97;
         overflow: hidden;
         white-space: nowrap;
         text-overflow: ellipsis;
         padding: 0 14px;
-        cursor: pointer;        
+        cursor: pointer;
       }
       .dept-text,
-      .person-text
-       {
+      .person-text {
         padding-right: 14px;
         width: 7%;
       }
       .time-text {
-        width: 11%;        
+        width: 11%;
       }
     }
   }
