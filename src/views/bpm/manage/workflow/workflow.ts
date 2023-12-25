ef5e453c362a7851e608a9baa29b05f66900d5c9
Mr.Yuan <15208170620m@sina.cn>
2023-12-25 08:39:48 +0800

    修改流程模型深色主题；

--- src/views/bpm/manage/workflow/workflow.ts
+++ src/views/bpm/manage/workflow/workflow.ts
@@ -1,4 +1,6 @@
 import { defHttp } from '/@/utils/http/axios';
+import { BasicColumn, FormSchema } from '@/components/Table';
+import { formatDate } from '/@/utils/dateUtil';
 
 enum Api {
   page = '/bpm/model/page',
@@ -23,3 +25,80 @@ export type ParamsType = {
   name: undefined | string;
   category: undefined | string;
 };
+
+const categorys = [
+  { value: '1', label: '默认' },
+  { value: '2', label: 'OA' },
+];
+
+export const searchFormSchema: FormSchema[] = [
+  {
+    field: `key`,
+    label: `流程标识`,
+    component: 'Input',
+    componentProps: { placeholder: '请输入流程标识' },
+    colProps: { span: 6 },
+  },
+  {
+    field: `name`,
+    label: `流程名称`,
+    component: 'Input',
+    componentProps: { placeholder: '请输入流程名称' },
+    colProps: { span: 6 },
+  },
+  {
+    field: `category`,
+    label: `流程分类`,
+    component: 'Select',
+    componentProps: {
+      options: categorys,
+    },
+    colProps: { span: 6 },
+  },
+];
+
+export const columns: BasicColumn[] = [
+  { title: '流程标识', dataIndex: 'key', width: 200 },
+  { title: '流程名称', dataIndex: 'name', width: 150 },
+  {
+    title: '流程分类',
+    dataIndex: 'category',
+    width: 80,
+  },
+  { title: '表单信息', dataIndex: 'formCustomCreatePath', width: 260 },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    width: 100,
+    customRender: ({ record }) => {
+      return formatDate(record.createTime, 'YYYY-MM-DD');
+    },
+  },
+  {
+    title: '最新部署的流程定义',
+    dataIndex: '',
+    children: [
+      {
+        title: '流程版本',
+        dataIndex: 'version',
+        width: 80,
+      },
+      {
+        title: '激活状态',
+        dataIndex: 'suspensionState',
+        width: 80,
+      },
+      {
+        title: '部署时间',
+        dataIndex: 'deploymentTime',
+        width: 100,
+        customRender: ({ record }) => {
+          if (record.processDefinition) {
+            return formatDate(record.processDefinition.deploymentTime, 'YYYY-MM-DD');
+          }
+          return '';
+        },
+      },
+    ],
+  },
+];
--- src/views/bpm/manage/workflow/workflow.vue
+++ src/views/bpm/manage/workflow/workflow.vue
@@ -1,196 +1,157 @@
 <template>
-  <div>
-    <!-- 搜索工作栏 -->
-    <WfSearchBox
-      ref="wfSearchBox"
-      @handle="handleQuery"
-      @reset="resetQuery"
-      @open="openForm('create')"
-      @import="importForm"
-    />
-
-    <!-- 搜索内容 -->
-    <div class="search-content">
-      <el-table v-loading="loading" :data="list">
-        <el-table-column label="流程标识" align="center" prop="key" width="200" />
-        <el-table-column label="流程名称" align="center" prop="name" width="200">
-          <template #default="scope">
-            <el-button type="primary" link @click="handleBpmnDetail(scope.row)">
-              <span>{{ scope.row.name }}</span>
-            </el-button>
-          </template>
-        </el-table-column>
-        <el-table-column label="流程分类" align="center" prop="category" width="100">
-          <template #default="scope">
-            <DictTags
-              v-show="scope.row.category"
-              :type="DICT_TYPE.BPM_MODEL_CATEGORY"
-              :value="scope.row.category"
-              mode="none"
-            />
-          </template>
-        </el-table-column>
-        <el-table-column label="表单信息" align="center" prop="formType" width="200">
-          <template #default="scope">
-            <el-button
-              v-if="scope.row.formType === 10"
-              type="primary"
-              link
-              @click="handleFormDetail(scope.row)"
-            >
-              <span>{{ scope.row.formName }}</span>
-            </el-button>
-            <el-button
-              v-else-if="scope.row.formType === 20"
-              type="primary"
-              link
-              @click="handleFormDetail(scope.row)"
-            >
-              <span>{{ scope.row.formCustomCreatePath }}</span>
-            </el-button>
-            <label v-else>暂无表单</label>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="创建时间"
-          align="center"
-          prop="createTime"
-          width="180"
-          :formatter="dateFormatter"
+  <BasicTable @register="registerTable">
+    <template #toolbar>
+      <a-button type="primary" @click="openForm('create')"> 新建流程 </a-button>
+      <a-button type="primary" @click="importForm"> 导入流程 </a-button>
+    </template>
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'action'">
+        <TableAction
+          :actions="[
+            {
+              label: '修改流程',
+              onClick: openForm.bind(null, 'update', record.id),
+            },
+            {
+              label: '设计流程',
+              onClick: handleDesign.bind(null, record),
+            },
+            {
+              label: '分配规则',
+              onClick: handleAssignRule.bind(null, record),
+            },
+            {
+              label: '发布流程',
+              onClick: handleDeploy.bind(null, record),
+            },
+            {
+              label: '流程定义',
+              onClick: handleDefinitionList.bind(null, record),
+            },
+            {
+              label: '删除',
+              onClick: handleDelete.bind(null, record.id),
+            },
+          ]"
         />
-        <el-table-column label="最新部署的流程定义" align="center">
-          <el-table-column
-            label="流程版本"
-            align="center"
-            prop="processDefinition.version"
-            width="100"
-          >
-            <template #default="scope">
-              <el-tag v-if="scope.row.processDefinition">
-                v{{ scope.row.processDefinition.version }}
-              </el-tag>
-              <el-tag v-else type="warning">未部署</el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="激活状态"
-            align="center"
-            prop="processDefinition.version"
-            width="85"
-          >
-            <template #default="scope">
-              <el-switch
-                v-if="scope.row.processDefinition"
-                v-model="scope.row.processDefinition.suspensionState"
-                :active-value="1"
-                :inactive-value="2"
-                @change="handleChangeState(scope.row)"
-              />
-            </template>
-          </el-table-column>
-          <el-table-column label="部署时间" align="center" prop="deploymentTime" width="180">
-            <template #default="scope">
-              <span v-if="scope.row.processDefinition">
-                {{ DateTools.format(scope.row.processDefinition.deploymentTime, 'YYYY-MM-DD') }}
-              </span>
-            </template>
-          </el-table-column>
-        </el-table-column>
-        <el-table-column label="操作" align="center" width="240" fixed="right">
-          <template #default="scope">
-            <el-button link type="primary" @click="openForm('update', scope.row.id)">
-              修改流程
-            </el-button>
-            <el-button link type="primary" @click="handleDesign(scope.row)"> 设计流程 </el-button>
-            <el-button link type="primary" @click="handleAssignRule(scope.row)">
-              分配规则
-            </el-button>
-            <el-button link type="primary" @click="handleDeploy(scope.row)"> 发布流程 </el-button>
-            <el-button link type="primary" @click="handleDefinitionList(scope.row)">
-              流程定义
-            </el-button>
-            <el-button link type="danger" @click="handleDelete(scope.row.id)"> 删除 </el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- 分页 -->
-      <Pagination
-        :total="total"
-        v-model:page="queryParams.pageNo"
-        v-model:limit="queryParams.pageSize"
-        @pagination="getList"
-      />
-    </div>
+      </template>
+      <template v-else-if="column.dataIndex === 'name'">
+        <a-button type="link" @click="handleBpmnDetail(record)">{{ record.name }}</a-button>
+      </template>
 
-    <!-- 表单弹窗：添加/修改流程 -->
-    <FormDialog ref="formDialogRef" @success="getList" v-model:visible="formDialogVisible" />
+      <template v-else-if="column.dataIndex === 'category'">
+        <a-tag v-if="record.processDefinition" color="processing">{{
+          record.category === '1' ? '默认' : 'OA'
+        }}</a-tag>
+      </template>
 
-    <!-- 表单弹窗：导入流程 -->
-    <ImportDialog ref="importDialogRef" @success="getList" v-model:visible="importDialogVisible" />
+      <template v-else-if="column.dataIndex === 'formCustomCreatePath'">
+        <a-button v-if="record.formType === 10" type="link" @click="handleFormDetail(record)">{{
+          record.formName
+        }}</a-button>
+        <a-button
+          v-else-if="record.formType === 20"
+          type="link"
+          @click="handleFormDetail(record)"
+          >{{ record.formCustomCreatePath }}</a-button
+        >
+        <label v-else>暂无表单</label>
+      </template>
 
-    <!-- 弹窗：表单详情 -->
-    <Dialog
-      title="表单详情"
-      v-model:visible="formDetailVisible"
-      :width="800"
-      :height="800"
-      :overflowY="'scroll'"
-      :showBtm="false"
-    >
-      <form-create :rule="formDetailPreview.rule" :option="formDetailPreview.option" />
-    </Dialog>
+      <template v-else-if="column.dataIndex === 'suspensionState'">
+        <a-switch
+          v-if="record.processDefinition"
+          :checked="getChecked(record)"
+          @change="handleChangeState(record)"
+        />
+      </template>
 
-    <!-- 弹窗：流程模型图的预览 -->
-    <Dialog
-      title="流程图"
-      v-model:visible="bpmnDetailVisible"
-      :width="800"
-      :height="800"
-      :overflowY="'scroll'"
-      :overflowX="'scroll'"
-      :showBtm="false"
-    >
-      <MyProcessViewer
-        key="designer"
-        v-model="bpmnXML"
-        :value="bpmnXML"
-        v-bind="bpmnControlForm"
-        :prefix="bpmnControlForm.prefix"
-      />
-    </Dialog>
-  </div>
+      <template v-else-if="column.dataIndex === 'version'">
+        <a-tag v-if="record.processDefinition" color="processing"
+          >v{{ record.processDefinition.version }}</a-tag
+        >
+        <a-tag v-else color="warning">未部署</a-tag>
+      </template>
+    </template>
+  </BasicTable>
+
+  <!-- 表单弹窗：添加/修改流程 -->
+  <FormDialog ref="formDialogRef" @success="reload" v-model:visible="formDialogVisible" />
+
+  <!-- 表单弹窗：导入流程 -->
+  <ImportDialog ref="importDialogRef" @success="reload" v-model:visible="importDialogVisible" />
+
+  <!-- 弹窗：表单详情 -->
+  <Dialog
+    title="表单详情"
+    v-model:visible="formDetailVisible"
+    :width="800"
+    :height="800"
+    :overflowY="'scroll'"
+    :showBtm="false"
+  >
+    <form-create :rule="formDetailPreview.rule" :option="formDetailPreview.option" />
+  </Dialog>
+
+  <!-- 弹窗：流程模型图的预览 -->
+  <Dialog
+    title="流程图"
+    v-model:visible="bpmnDetailVisible"
+    :width="800"
+    :height="800"
+    :overflowY="'scroll'"
+    :overflowX="'scroll'"
+    :showBtm="false"
+  >
+    <MyProcessViewer
+      key="designer"
+      v-model="bpmnXML"
+      :value="bpmnXML"
+      v-bind="bpmnControlForm"
+      :prefix="bpmnControlForm.prefix"
+    />
+  </Dialog>
 </template>
 <script lang="ts" setup>
-  import { DICT_TYPE } from '@/utils/dict';
-  import { DateTools, dateFormatter } from '@/utils/dateUtil';
-  import { ref, reactive, onMounted } from 'vue';
-  import { useRouter } from 'vue-router';
-  import WfSearchBox from '/@/components/Framework/WorkFlow/WfSearchBox.vue';
-  import DictTags from '@/components/Framework/Tag/DictTag/DictTags.vue';
-  import Pagination from '@/components/Framework/Pagination/Pagination.vue';
+  import { reactive, ref, toRaw, onMounted } from 'vue';
   import { useMessage } from '/@/hooks/web/useMessage';
+  import { useI18n } from '@/hooks/web/useI18n';
+  import { BasicTable, useTable, TableAction } from '@/components/Table';
+  import { useRouter } from 'vue-router';
+  import { columns, searchFormSchema } from './workflow';
+  import * as ModelApi from '@/api/bpm/model';
+
   import Dialog from '@/components/Framework/Modal/Dialog.vue';
   import FormDialog from './formDialog.vue';
   import ImportDialog from './importDialog.vue';
-  import { getModelPage, getModel, deployModel, deleteModel } from '@/api/bpm/model';
   import { MyProcessViewer } from '@/components/Bpm/package';
 
-  defineOptions({ name: 'WorkFlow' });
-  const message = useMessage(); // 消息弹窗
+  const { t } = useI18n();
   const router = useRouter();
+  const message = useMessage(); // 消息弹窗
 
-  const loading = ref(true); // 列表的加载中
-  const total = ref(0); // 列表的总页数
-  const list = ref([]); // 列表的数据
-  const wfSearchBox = ref(); // 搜索的表单
-
-  const queryParams = reactive({
-    pageNo: 1,
-    pageSize: 10,
-    key: undefined,
-    name: undefined,
-    category: undefined,
+  const [registerTable, { reload }] = useTable({
+    title: '流程模型',
+    api: ModelApi.getModelPage,
+    columns,
+    formConfig: { labelWidth: 120, schemas: searchFormSchema },
+    useSearchForm: true,
+    showTableSetting: true,
+    bordered: true,
+    actionColumn: {
+      width: 400,
+      title: t('common.operate'),
+      dataIndex: 'action',
+      fixed: 'right',
+    },
   });
+
+  const getChecked = (record) => {
+    if (record.processDefinition.suspensionState === 1) {
+      return true;
+    }
+    return false;
+  };
 
   /** 流程图的详情按钮操作 */
   const bpmnDetailVisible = ref(false);
@@ -222,51 +183,18 @@
     formDialogRef.value.open(type, id);
   };
 
-  /** 搜索按钮操作 */
-  const handleQuery = (params) => {
-    queryParams.pageNo = 1;
-    getList(params);
-  };
-
-  /** 重置按钮操作 */
-  const resetQuery = (params) => {
-    const formRef = wfSearchBox.value.$refs.queryFormRef;
-    formRef.resetFields();
-    handleQuery(params);
-  };
-
-  // 查询流程表格列表数据
-  const getList = async (params) => {
-    loading.value = true;
-    try {
-      getModelPage({
-        pageNo: queryParams.pageNo,
-        pageSize: queryParams.pageSize,
-        key: params ? params.key : '',
-        name: params ? params.name : '',
-        category: params ? params.category : '',
-      }).then((data) => {
-        list.value = data.list;
-        total.value = data.total;
-      });
-    } finally {
-      loading.value = false;
-    }
-  };
-
   // 执行删除流程记录操作
-  const handleDelete = async (id: number) => {
+  const handleDelete = async (id) => {
     try {
       // 删除的二次确认
       await message.delConfirm(`请确认是否删除此流程数据项？`);
-
       // 调用集维后端接口，删除流程相应数据 TODO
-      deleteModel(id).then(() => {
+      ModelApi.deleteModel(id).then(() => {
         // 刷新列表
-        getList();
+        reload();
       });
     } catch (e) {
-      // console.error(e);
+      console.error(e);
     }
   };
 
@@ -278,14 +206,11 @@
       const id = row.id;
       const statusState = state === 1 ? '激活' : '挂起';
       const content = '请确认是否' + statusState + '流程名字为"' + row.name + '"的数据项?';
-
       // 确认是否更新流程激活状态
       await message.confirm(content);
-
       // 调用集维后端接口，激活该流程状态 TODO
-
       // 刷新列表
-      await getList();
+      await reload();
     } catch {
       // 取消后，进行恢复按钮
       row.processDefinition.suspensionState = state === 1 ? 2 : 1;
@@ -297,13 +222,12 @@
     try {
       // 删除的二次确认
       await message.confirm('请确认是否部署该流程？');
-
-      deployModel(row.id).then(() => {
+      ModelApi.deployModel(row.id).then(() => {
         // 刷新列表
-        getList();
+        reload();
       });
     } catch (e) {
-      // console.error(e);
+      console.error(e);
     }
   };
 
@@ -337,22 +261,8 @@
 
   const handleBpmnDetail = async (row) => {
     bpmnDetailVisible.value = true;
-
-    getModel(row.id).then((data) => {
+    ModelApi.getModel(row.id).then((data) => {
       bpmnXML.value = data.bpmnXml;
     });
   };
-
-  /** 初始化 **/
-  onMounted(() => {
-    getList();
-  });
 </script>
-<style scoped>
-  .search-content {
-    width: calc(100% - 25px);
-    height: calc(100vh - 150px);
-    margin: 15px 15px 10px 10px;
-    background: #fefefe;
-  }
-</style>
