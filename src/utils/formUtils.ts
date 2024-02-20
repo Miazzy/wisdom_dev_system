38c266e031e42ef9992d62cafcc3620f4c954203
miazzy <qq695926920@163.com>
2024-02-20 11:16:52 +0800

    fix: 修复#13676，【集维标准版】电量管理-日报填报，保存提交时建议定位到必填项

--- src/utils/formUtils.ts
+++ src/utils/formUtils.ts
@@ -8,3 +8,25 @@ export const setReactiveValues = (fstate, data: Object) => {
   }
 };
 
+export const scrollToInValidField = (formInstance, fields, index = 0) => {
+  if (fields && fields.errorFields) {
+    try {
+      let field = null;
+      if (fields.errorFields.length > index) {
+        field = fields.errorFields[index];
+      } else if (fields.errorFields.length > 2) {
+        field = fields.errorFields[2];
+      } else if (fields.errorFields.length > 1) {
+        field = fields.errorFields[1];
+      } else if (fields.errorFields.length > 0) {
+        field = fields.errorFields[0];
+      }
+      if (Reflect.has(field, 'name')) {
+        const name = field['name'];
+        formInstance?.scrollToField(name[0]);
+      }
+    } catch (error) {
+      //
+    }
+  }
+}
