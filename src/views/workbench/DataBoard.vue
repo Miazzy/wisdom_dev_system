a2b043535c48a8fac1a57fdcec78e4dc114a88a8
zhangzr <zhangzr03@tongwei.com>
2024-02-23 10:20:10 +0800

    工作台

--- src/views/workbench/DataBoard.vue
+++ src/views/workbench/DataBoard.vue
@@ -1,105 +1,175 @@
 <template>
-  <a-card title="数据看板" :bordered="false">
+  <a-card title="数据看板" :bordered="false" @click="handleOpenDialog()">
     <template #extra>
       <Icon icon="ion:settings-outline" :size="17" />
     </template>
     <div class="card-content">
-      <div class="data-board-item">
+      <!-- <div class="data-board-item">
         <div class="label-text">并网容量(MWp)</div>
         <div class="value-text">563.33161</div>
-        <!-- <div class="percentage-box">
-          <div class="pLabel-text">同比昨日</div>
-          <div class="pValue-text up">1.6%</div>
-        </div> -->
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">日发电量(kWh)</div>
-        <div class="value-text">140349.07</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">实时功率(kW)</div>
-        <div class="value-text">162588.25</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">月上网电量(万kWh)</div>
-        <div class="value-text">2932.732</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">年上网电量(万kWh)</div>
-        <div class="value-text">44179.5866</div>
-      </div>
-
-      <div class="data-board-item">
-        <div class="label-text">月计划完成率(%)</div>
-        <div class="value-text">81.46</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">年计划完成率(%)</div>
-        <div class="value-text">71.32</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">月损失电量(万kWh)</div>
-        <div class="value-text">289.1381</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">待闭环工单(笔)</div>
-        <div class="value-text">0</div>
-      </div>
-      <div class="data-board-item">
-        <div class="label-text">故障告警(台)</div>
-        <div class="value-text">6</div>
+      </div> -->
+      <div class="data-board-item" v-for="(n, index) in formState.dataBoard" :key="index">
+        <div class="label-text">{{ n.fullName }}</div>
+        <div class="value-text">{{ 11111 }}</div>
       </div>
     </div>
   </a-card>
+
+  <DataSelectDialog
+    title="指标库"
+    v-model:visible="openDialog"
+    :value="formState.dataBoard"
+    :tdata="indexTreeData"
+    :tfields="{ key: 'value', title: 'label' }"
+    :width="800"
+    :height="600"
+    @cancel="receiverCancel"
+    @confirm="receiverConfirm"
+  />
 </template>
 <script lang="ts" setup>
+  import { ref, reactive, onMounted } from 'vue';
   import Icon from '@/components/Icon/Icon.vue';
+  import DataSelectDialog from '@/components/Framework/Modal/DataSelectDialog.vue';
+  import { TreeSelectProps, message } from 'ant-design-vue';
+  import { getIndexTreeData, saveDataBoard, getDataBoard } from './data';
+
+  const openDialog = ref(false);
+  const indexTreeData = ref<TreeSelectProps['treeData']>();
+  const formState: any = reactive({
+    dataBoard: [],
+  });
+
+  onMounted(async () => {
+    queryDataBoardByUserId();
+  });
+
+  //当前登录人的数据看板设置
+  const queryDataBoardByUserId = async () => {
+    await getDataBoard({}).then((rsp) => {
+      if (rsp) {
+        formState.dataBoard = [];
+        rsp.forEach((e) => {
+          formState.dataBoard.push({
+            value: e.id,
+            label: e.name,
+            parentId: e.parentId,
+            name: e.name,
+            fullName: e.fullName,
+          });
+        });
+      }
+    });
+  };
+
+  //查询树
+  const queryIndexTreeData = async () => {
+    const typeSubList = await getIndexTreeData({});
+    indexTreeData.value = dig(typeSubList.result, true);
+  };
+
+  function dig(data, selectIndex) {
+    const list: TreeSelectProps['treeData'] = [];
+    for (let i = 0; i < data.length; i++) {
+      const item = data[i];
+
+      const treeNode: TreeSelectProps['treeData'][number] = {
+        label: item.name,
+        value: item.id,
+        parentId: item.parentId,
+        disabled: selectIndex ? item.type !== 'index' : false,
+      };
+
+      if (item.children && item.children.length > 0) {
+        treeNode.children = dig(item.children, selectIndex);
+      }
+      list.push(treeNode);
+    }
+    return list;
+  }
+
+  //打开
+  const handleOpenDialog = () => {
+    openDialog.value = true;
+    queryIndexTreeData();
+  };
+
+  //清空
+  const receiverCancel = () => {
+    openDialog.value = false; // 关闭弹框
+  };
+
+  //保存
+  const receiverConfirm = (nodeList, allList) => {
+    openDialog.value = false;
+    let dataList: any = [];
+    if (nodeList && nodeList.length > 0) {
+      nodeList.forEach((e) => {
+        dataList.push({
+          indexStorehouseId: e.value,
+        });
+      });
+      saveDataBoard(dataList).then(() => {
+        message.success('操作成功');
+        queryDataBoardByUserId();
+      });
+    }
+  };
 </script>
 
 <style lang="less" scoped>
   .card-content {
     height: 278px;
-    padding-top: 16px;
     margin-right: -13px;
+    padding-top: 16px;
+
     .data-board-item {
-      float: left;
       width: 170px;
       height: 100px;
-      border-radius: 2px;
       margin: 0 12px 16px 0;
-      padding-left: 20px;
       padding-top: 14px;
+      padding-left: 20px;
+      float: left;
+      border-radius: 2px;
       line-height: 1;
+
       .label-text {
-        font-size: 13px;
         margin-bottom: 8px;
+        font-size: 13px;
       }
+
       .value-text {
+        margin-bottom: 12px;
         font-size: 24px;
         font-weight: 500;
-        margin-bottom: 12px;
       }
+
       .percentage-box {
         display: flex;
         align-items: center;
         font-size: 12px;
       }
+
       .pLabel-text {
         margin-right: 11px;
       }
+
       .pValue-text {
         &.up {
           color: #ff4d4f;
         }
+
         &.up::before {
           content: '▲';
           display: inline-block;
           transform: scale(0.6);
           color: #ff4d4f;
         }
+
         &.down {
           color: #52c41a;
         }
+
         &.down::before {
           content: '▼';
           display: inline-block;
