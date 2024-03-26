f35c7f8d882497cc73a2fd86f31edf8d16aa9825
miazzy <qq695926920@163.com>
2024-03-26 14:20:52 +0800

    fix: #15656, 【前端框架】所有按钮的图标都不显示

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -1,9 +1,10 @@
 import type { App } from 'vue';
-// import { Button } from './Button';
+import { Button } from './Button';
 // import { Input, Layout } from 'ant-design-vue';
 // import VXETable from 'vxe-table';
 
 export function registerGlobComp(app: App) {
-  // app.use(Input).use(Layout);
+  app.use(Button);
+  //.use(Input).use(Layout);
   //.use(VXETable);
 }
