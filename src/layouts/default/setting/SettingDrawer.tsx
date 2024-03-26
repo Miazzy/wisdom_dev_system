201b4d10d51c5b03e13154db5580ccbd677833ef
miazzy <qq695926920@163.com>
2024-03-26 15:12:30 +0800

    fix: #15689 , 调整Tab栏报错问题，【前端框架】页面出现打不开的情况

--- src/layouts/default/setting/SettingDrawer.tsx
+++ src/layouts/default/setting/SettingDrawer.tsx
@@ -1,25 +1,14 @@
-import { defineComponent, unref, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { BasicDrawer } from '/@/components/Drawer/index';
 import { Divider, Spin } from 'ant-design-vue';
 import { SettingFooter, ThemePicker } from './components';
-
 import { AppDarkModeToggle } from '/@/components/Application';
-import { TriggerEnum } from '/@/enums/menuEnum';
 
-import { useRootSetting } from '/@/hooks/setting/useRootSetting';
-import { useMenuSetting } from '/@/hooks/setting/useMenuSetting';
-import { useI18n } from '/@/hooks/web/useI18n';
-import { getMenuTriggerOptions } from './enum';
-
-const { t } = useI18n();
 const spinStatus = ref(false);
 
 export default defineComponent({
   name: 'SettingDrawer',
   setup(_, { attrs }) {
-    const { getShowDarkModeToggle } = useRootSetting();
-    const { getTrigger, getSplit } = useMenuSetting();
-
     function handleThemeChange(mode, status) {
       spinStatus.value = status;
     }
@@ -28,38 +17,11 @@ export default defineComponent({
       return <ThemePicker />;
     }
 
-    /**
-     * @description:
-     */
-    function renderFeatures() {
-      let triggerDef = unref(getTrigger);
-
-      const triggerOptions = getMenuTriggerOptions(unref(getSplit));
-      const some = triggerOptions.some((item) => item.value === triggerDef);
-      if (!some) {
-        triggerDef = TriggerEnum.FOOTER;
-      }
-
-      return (
-        <>
-          <div></div>
-        </>
-      );
-    }
-
     return () => (
-      <BasicDrawer
-        {...attrs}
-        title={t('layout.setting.drawerTitle')}
-        width={330}
-        class="setting-drawer"
-      >
-        {unref(getShowDarkModeToggle) && <Divider>{() => t('layout.setting.darkMode')}</Divider>}
-        {unref(getShowDarkModeToggle) && (
-          <AppDarkModeToggle class="mx-auto" onChange={handleThemeChange} />
-        )}
+      <BasicDrawer {...attrs} title="项目配置" width={330} class="setting-drawer">
+        {<Divider>{() => '主题'}</Divider>}
+        {<AppDarkModeToggle class="mx-auto" onChange={handleThemeChange} />}
         {renderTheme()}
-        {renderFeatures()}
         <Divider />
         <SettingFooter />
         <div style="width: 100%; height: calc(50vh - 260px); "></div>
