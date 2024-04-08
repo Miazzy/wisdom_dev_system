e2dc158b3e616647d78c3fb9bdc13ff8e11b63f5
miazzy <qq695926920@163.com>
2024-04-08 14:08:46 +0800

    fix: #14965, 【前端框架】增加PC端数据范围选择控件

---
+++ src/components/Framework/InputGroup/InputGroup.vue
@@ -0,0 +1,12 @@
+<template>
+  
+</template>
+<script lang="ts" setup>
+  import { onMounted, ref, watch, defineProps, defineEmits } from 'vue';
+
+  onMounted(() => {
+    //
+  });
+</script>
+<style lang="less" scoped>
+</style>
