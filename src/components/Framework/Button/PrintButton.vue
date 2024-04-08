b6ab8a56d5450302b887221b9b138ab83aab4d0b
lwr <610507101@qq.com>
2024-04-08 13:48:48 +0800

    功能开发 #15809打印组件

---
+++ src/components/Framework/Button/PrintButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <a-button class="print-btn" @click="printPage">打印</a-button>
+</template>
+<script lang="ts" setup>
+  import printJS from 'print-js';
+
+  const props = defineProps({
+    config: {
+      type: Object,
+      default: {},
+    },
+  });
+  const defaultConfig = {
+    printable: 'app',
+    type: 'html',
+    maxWidth: 1100,
+    scanStyles: false,
+    css: './css/printStyle.css',
+  };
+  // 打印
+  const printPage = () => {
+    let printJSConfig = {};
+    Object.assign(printJSConfig, defaultConfig, props.config);
+    printJS(printJSConfig);
+  };
+</script>
