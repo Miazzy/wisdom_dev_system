9136af8c9d251af5b52693b1a328aaa04b0afd35
miazzy <qq695926920@163.com>
2024-04-11 14:54:00 +0800

    fix: #14438, 【前端框架】PC端框架优化：配合界面元素权限管理，匹配界面权限；【4月完成】

--- src/directives/permission.ts
+++ src/directives/permission.ts
@@ -4,15 +4,19 @@
  * @Example v-auth="RoleEnum.TEST"
  */
 import type { App, Directive, DirectiveBinding } from 'vue';
+import { useUserStore } from '/@/store/modules/user';
 
-import { usePermission } from '/@/hooks/web/usePermission';
+const userStore = useUserStore();
+const userInfo = userStore.getUserInfo as { permissions?: [] };
+const permissionMap = new Map<string, any>();
 
 function isAuth(el: Element, binding: any) {
-  const { hasPermission } = usePermission();
-
+  if (permissionMap.size == 0) {
+    userInfo?.permissions?.map((key) => permissionMap.set(key, true));
+  };
   const value = binding.value;
   if (!value) return;
-  if (!hasPermission(value)) {
+  if (!permissionMap.has(value)) {
     el.parentNode?.removeChild(el);
   }
 }
@@ -21,12 +25,18 @@ const mounted = (el: Element, binding: DirectiveBinding<any>) => {
   isAuth(el, binding);
 };
 
+const updated = (el: Element, binding: DirectiveBinding<any>) => {
+  isAuth(el, binding);
+};
+
 const authDirective: Directive = {
   mounted,
+  updated,
 };
 
 export function setupPermissionDirective(app: App) {
   app.directive('auth', authDirective);
+  app.directive('permission', authDirective);
 }
 
 export default authDirective;
