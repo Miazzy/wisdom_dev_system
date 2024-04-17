7883e78c76eadd7d2bbd693b1dbb12055fc83455
Yuanpeng <1414561742@qq.com>
2024-04-17 20:25:59 +0800

    修改附件下载。

--- src/api/infra/file/index.ts
+++ src/api/infra/file/index.ts
@@ -40,7 +40,7 @@ export const attachmentPreviewDomain = () => {
   return previewdomain;
 };
 
-export const attachmentDownloadUrl = (url) => {
+export const attachmentPreviewUrl = (url,name) => {
   const pictureTypes = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.ico', '.jfif', '.webp'];
   const arrays = pictureTypes.filter((item) => url.indexOf(item) > 0);
   let downloaddomain = arrays.length == 0 ? 'http://192.168.0.200' : 'http://139.9.148.32'; //测试环境下载域名
@@ -49,14 +49,28 @@ export const attachmentDownloadUrl = (url) => {
     downloaddomain = 'http://10.8.111.203'; //下载域名
   }
   downloaddomain = downloaddomain + ':48080'; //下载域名
+  let previewUrl = downloaddomain + getUrl(url);
+  if(name){
+    previewUrl = previewUrl+'&fullfilename='+name;
+  }
+  return previewUrl;
+};
+
+export const attachmentDownloadUrl = (url) => {
+  let downloaddomain = 'http://139.9.148.32'; //测试环境下载域名
+  const isDev = process.env.NODE_ENV === 'development';
+  if (isDev) {
+    downloaddomain = 'http://10.8.111.203'; //下载域名
+  }
+  downloaddomain = downloaddomain + ':48080'; //下载域名
   return downloaddomain + getUrl(url);
 };
 
-export const attachmentPreview = (url) => {
+export const attachmentPreview = (url,name) => {
   return (
     attachmentPreviewDomain() +
     '/onlinePreview?url=' +
-    encodeURIComponent(btoa(encodeURI(attachmentDownloadUrl(url))))
+    encodeURIComponent(btoa(encodeURI(attachmentPreviewUrl(url,name))))
   );
 };
 
