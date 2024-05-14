bb9513333cf77075536bb50624cd8df4320bd2cd
lwr <610507101@qq.com>
2024-05-14 09:32:40 +0800

    优化：#16035驾驶舱页面优化，地图遮罩

--- src/components/Framework/Chart/Indicator.vue
+++ src/components/Framework/Chart/Indicator.vue
@@ -54,7 +54,6 @@
     padding: 0.02rem 0.04rem;
     background-color: transparent;
     color: white;
-    font-family: Arial, sans-serif;
     text-align: center;
   }
 
@@ -67,6 +66,7 @@
     margin: 0.02rem 0;
     color: #1890FF;
     font-size: 0.22rem;
+    font-weight: 500;
   }
 
   .info {
