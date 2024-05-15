2a9d2bce744be778fe6303287667114a197df18e
zhangzr <zhangzr03@tongwei.com>
2024-05-15 11:23:21 +0800

    决策分析接口定义增加数据字典

--- src/api/da/common/index.ts
+++ src/api/da/common/index.ts
@@ -2,5 +2,14 @@ import { defHttp } from '@/utils/http/axios';
 
 // 天气
 export function getWeather(params) {
-  return defHttp.get({ url: '/system/weather/get', params: {rectangle: '104.06,30.65', ...params} }, { isTransformResponse: false });
+  return defHttp.get(
+    { url: '/system/weather/get', params: { rectangle: '104.06,30.65', ...params } },
+    { isTransformResponse: false },
+  );
 }
+
+//获取有效数据字典
+export const getDictDataMap = (params: any) => {
+  const requestParams = { url: '/system/dict-data/getDictList', params };
+  return defHttp.get<any>(requestParams, {});
+};
