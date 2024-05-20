5e1e4db52fba22d2735cea005ca07da67d1a9282
Yuanpeng <1414561742@qq.com>
2024-05-20 08:42:52 +0800

    功能开发 #16263 【工作台】增加“资讯”页签

--- src/views/workbench/Notice.vue
+++ src/views/workbench/Notice.vue
@@ -1,23 +1,43 @@
 <template>
-  <a-card title="通知公告" :bordered="false">
+  <a-card title="信息公告" :bordered="false">
     <template #extra>
       <span @click="toMorePage">更多 &gt;</span>
     </template>
     <div class="card-content">
-      <ul class="notice-list-box">
-        <li
-          class="notice-item"
-          @click="toNotice(item)"
-          v-for="(item, index) in noticeList"
-          :key="index"
-        >
-          <span class="item-dot"></span>
-          <span class="title-text" :class="{ isRead: item.isRead }">{{ item.title }}</span>
-          <span class="dept-text">{{ item.dept }}</span>
-          <span class="person-text">{{ item.person }}</span>
-          <span class="time-text">{{ item.time }}</span>
-        </li>
-      </ul>
+      <a-tabs v-model:activeKey="activeKey" @change="handleTabChange">
+        <a-tab-pane key="1" tab="通知公告">
+          <ul class="notice-list-box">
+            <li
+              class="notice-item"
+              @click="toNotice(item)"
+              v-for="(item, index) in noticeList"
+              :key="index"
+            >
+              <span class="item-dot"></span>
+              <span class="title-text" :class="{ isRead: item.isRead }">{{ item.title }}</span>
+              <span class="dept-text">{{ item.dept }}</span>
+              <span class="person-text">{{ item.person }}</span>
+              <span class="time-text">{{ item.time }}</span>
+            </li>
+          </ul>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="资讯" force-render>
+          <ul class="notice-list-box">
+            <li
+              class="notice-item"
+              @click="toInformation(item)"
+              v-for="(item, index) in informationList"
+              :key="index"
+            >
+              <span class="item-dot"></span>
+              <span class="title-text">{{ item.title }}</span>
+              <span class="dept-text">{{ item.type }}</span>
+              <span class="person-text">{{ item.person }}</span>
+              <span class="time-text">{{ item.time }}</span>
+            </li>
+          </ul>
+        </a-tab-pane>
+      </a-tabs>
     </div>
   </a-card>
 </template>
@@ -33,9 +53,20 @@
     { key: 'announcement', url: '/oa/info/officedoc/view' },
   ]);
   const noticeList = ref([]);
+  const informationList = ref([]);
 
   const toMorePage = () => {
     addTabPage(`/oa/info/query/index`, '信息查询');
+  };
+
+
+  const activeKey = ref('1');
+  const handleTabChange = (item) => {
+    if (item === '1') {
+      getNoticeAndAnnouncementPage();
+    } else if (item === '2') {
+      getInformation();
+    } 
   };
 
   const toNotice = (item) => {
@@ -50,6 +81,14 @@
       addTabPage(element.url, '通知公告详情', params);
 
       item.isRead = 1; //设置为已读
+    }
+  };
+
+  const toInformation = (item) => {
+    if(item.source === '1'){
+      window.open(item.url);
+    }else{
+      addTabPage('/oa/info/informationquery/index', '资讯查询');
     }
   };
 
@@ -69,6 +108,29 @@
           time: DateUtil.formatToDate(e.finishedDate, 'YYYY-MM-DD HH:mm'),
           isRead: e.isRead,
           businessType: e.businessType,
+        });
+      });
+    }
+  };
+
+  const getInformation = async () => {
+    const information = await WorkbenchApi.getInformationPage({
+      pageNo: 1,
+      pageSize: 7,
+    });
+    if (information) {
+      informationList.value.splice(0, informationList.value.length);
+      information.list.forEach((e) => {
+        informationList.value.push({
+          id: e.id,
+          title: e.subject,
+          type: e.typeText,
+          source: e.source,
+          url: e.url,
+          person: e.personMemberName,
+          time: DateUtil.formatToDate(e.finishedDate, 'YYYY-MM-DD HH:mm'),
+          // isRead: e.isRead,
+          // businessType: e.businessType,
         });
       });
     }
