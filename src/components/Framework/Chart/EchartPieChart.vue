d31b45813c11d35cc17cc9075d64e1738f1d1ec4
lwr <610507101@qq.com>
2024-05-30 10:51:52 +0800

    功能优化：#16477【驾驶舱】驾驶舱环形图 更换成echarts组件，

--- src/components/Framework/Chart/EchartPieChart.vue
+++ src/components/Framework/Chart/EchartPieChart.vue
@@ -1,121 +1,179 @@
 <template>
-  <div
-    id="echarts-pie-container"
-    class="echarts-pie-container"
-    :style="`width: ${typeof props.width == 'number' ? props.width + 'px' : props.width}; height: ${
-      typeof props.height == 'number' ? props.height + 'px' : props.height
-    };`"
-  ></div>
+  <div :class="{'flex-layout': legendPosition==='right'}">
+    <div :id="props.id" ref="chart"
+      :style="`width: ${typeof props.width == 'number' ? props.width + 'px' : props.width}; height: ${typeof props.height == 'number' ? props.height + 'px' : props.height};`"></div>
+  </div>
 </template>
-<script lang="ts" setup>
-  import { nextTick, onMounted, watch } from 'vue';
-  import * as echarts from 'echarts';
 
-  // 定义属性
+<script lang="ts" setup>
+  import * as echarts from 'echarts';
+  import { onMounted, watch, PropType, nextTick, ref } from 'vue';
+
   const props = defineProps({
-    data: { type: Array, default: [] as any[] },
-    width: { type: [Number, String], default: 375 },
-    height: { type: [Number, String], default: 375 },
-    colors: { type: Array, default: ['#55D187', '#ED6F6F', '#0960bd', '#fc344a', '#47BDAF', '#0065cc', '#94f7f9'] as any[] },
-    showLabel: { type: Boolean, default: false },
-    unit: { type: [String], default: '' },
+    width: { type: [Number, String], default: 600 },
+    height: { type: [Number, String], default: 300 },
+    data: Array,
+    id: { type: String },
+    showLabel: { type: Boolean, default: false }, // 是否显示饼图图形旁边的文字标签
+    roseType: { type: [String, Boolean], default: 'radius' }, // 是否展示成南丁格尔图，通过半径区分数据大小 radius/area
+    radius: { type: Array as PropType<Array<number | string>>, default: [30, 100] },
+    labelFormatter: { type: [String, Function], default: '{b}: {c}' },
+    showLegend: { type: Boolean, default: false }, // 是否显示图例
+    legendOptions: { type: Object, default: {} }, // 图例配置
+    pieCenter: { type: Array as PropType<Array<number | string>>, default: ['50%', '50%'] }, // 饼图中心位置
   });
 
-  // 监听数据变化
+  const emit = defineEmits(['clickItem']);
+
+  const colors = ['#0380FF', '#8FD0F4', '#697898', '#4ADECA', '#F5CC6B'];
+
+  const setupData = () => {
+    const chartDom = document.getElementById(props.id);
+    let myChart = echarts.getInstanceByDom(chartDom);
+    if (myChart == undefined) {
+      myChart = echarts.init(chartDom);
+    }
+    const option = {
+      color: colors,
+      legend: {
+        show: props.showLegend,
+        bottom: '6%',
+        left: 'center',
+        orient: 'horizontal',
+        icon: 'circle',
+        itemWidth: 8,
+        itemHeight: 8,
+        textStyle: {
+          color: 'rgba(255,255,255,0.6)',
+          fontSize: 10,
+          height: 10,
+          lineHeight: 10,
+          rich: {},
+          padding: [4,0,0,0]
+        },
+        ...props.legendOptions
+      },
+      tooltip: {
+        show: true,
+        textStyle: {
+          color: '#fff',
+        },
+        backgroundColor: 'rgb(50,175,255, 15%)',
+        borderColor: 'transparent',
+        formatter: '{b}: {c}',
+        position: function (point, params, dom, rect, size) {
+          var x = 0; // x坐标位置
+          var y = 0; // y坐标位置
+          var pointX = point[0];
+          var pointY = point[1];
+          var boxWidth = size.contentSize[0];
+          var boxHeight = size.contentSize[1];
+          if (boxWidth > pointX) {
+            x = pointX + 10;
+          } else {
+            x = pointX - boxWidth - 10;
+          }
+          if (boxHeight > pointY) {
+            y = 5;
+          } else {
+            y = pointY - boxHeight;
+          }
+          return [x, y];
+        },
+      },
+      series: [
+        {
+          name: '',
+          type: 'pie',
+          radius: props.radius,
+          center: props.pieCenter,
+          roseType: props.roseType,
+          itemStyle: {
+            borderRadius: 0,
+          },
+          data: props.data,
+          label: {
+            show: props.showLabel,
+            formatter: props.labelFormatter,
+            overflow: 'break',
+            textBorderType: 'none',
+            color: '#BAC3C4',
+          },
+        },
+      ],
+    };
+    option && myChart.setOption(option);
+  };
+
   watch(
     () => props.data,
-    () => {
-      nextTick(() => {
-        createChart();
-      });
+    (newData) => {
+      setupData();
     },
     {
       deep: true,
     },
   );
 
-  // 创建图表
-  const createChart = () => {
-    var chartDom = document.getElementById('echarts-pie-container');
-    var myChart = echarts.init(chartDom);
-    var option;
-    var datas = [props.data];
+  watch(
+    () => props.height,
+    () => {
+      setTimeout(() => {
+        const chartDom = document.getElementById(props.id);
+        let myChart = echarts?.getInstanceByDom(chartDom);
+        myChart?.resize();
+      }, 100);
+    },
+  );
 
-    option = {
-      series: datas.map(function (data, idx) {
-        var top = idx * 33.3;
-        return {
-          type: 'pie',
-          radius: [40, 85],
-          top: top + '%',
-          height: '55%',
-          left: 'center',
-          width: props.width,
-          itemStyle: {
-            color: function (params) {
-              return props.colors[params.dataIndex % props.colors.length];
-            },
-            borderColor: 'transparent',
-            borderWidth: 1,
-          },
-          label: {
-            show: props.showLabel,
-            alignTo: 'edge',
-            formatter: `{name|{b}}\n{time|{c} ${props.unit}}`,
-            minMargin: 5,
-            edgeDistance: 10,
-            lineHeight: 15,
-            fontSize: 14,
-            color: '#fefefe',
-            rich: {
-              time: {
-                fontSize: 14,
-                color: '#fefefe',
-              },
-            },
-          },
-          labelLine: {
-            length: 10,
-            length2: 0,
-            maxSurfaceAngle: 80,
-          },
-          labelLayout: function (params) {
-            const isLeft = params.labelRect.x < myChart.getWidth() / 2;
-            const points = params.labelLinePoints;
-            if (typeof points == 'undefined' || points == null) {
-              return { labelLinePoints: points };
-            } else {
-              points[2][0] = isLeft
-                ? params.labelRect.x
-                : params.labelRect.x + params.labelRect.width;
-              return { labelLinePoints: points };
-            }
-          },
-          data: data,
-        };
-      }),
-    };
-
-    option && myChart.setOption(option);
-  };
-
-  const emit = defineEmits(['clickItem']);
-
-  // 创建地图并绘制点位
   onMounted(() => {
-    createChart();
-    nextTick(()=>{
-      const chartDom = document.getElementById('echarts-pie-container');
+    setupData();
+    nextTick(() => {
+      const chartDom = document.getElementById(props.id);
       let myChart = echarts.getInstanceByDom(chartDom);
-      myChart.on('click', (params)=> {
+      myChart.on('click', (params) => {
         emit('clickItem', params);
-      })
-    })
+      });
+    });
   });
 </script>
-<style>
-  .echarts-pie-container {
+<style lang="less" scoped>
+  .flex-layout {
+    display: flex;
+    align-items: center;
     width: 100%;
-    height: 100%;
+    .right-legend-box {
+      padding-left: 0.1rem;
+      flex: 1;
+      .legend-list {
+        display: flex;
+        flex-wrap: wrap;
+        margin-bottom: 0;
+        .legend-item {
+          width: 50%;
+          display: flex;
+          .item-left {
+            font-size: 0.12rem;
+            .dot-icon {
+              display: inline-block;
+              width: 0.1rem;
+              height: 0.1rem;
+              border-radius: 50%;
+              margin-right: 0.12rem;
+            }
+          }
+          .item-right {
+            .name {
+              font-size: 0.12rem;
+              color: rgba(255, 255, 255, 0.6);
+            }
+            .value {
+              font-size: 0.14rem;
+              color: #fff;
+            }
+          }
+        }
+      }
+    }
   }
 </style>
