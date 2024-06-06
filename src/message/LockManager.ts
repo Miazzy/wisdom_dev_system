83bb3f303fb9f5bd2826733656e89d69d46f129e
miazzy <qq695926920@163.com>
2024-06-06 14:22:57 +0800

    fix: #16662, 【前端框架】添加锁管理器，可以进行全局上锁解锁操作

---
+++ src/message/LockManager.ts
@@ -0,0 +1,55 @@
+interface LockManagerInterface {
+  acquireLock(lockName: string): Promise<void>;
+  releaseLock(lockName: string): Promise<void>;
+  hasLock(lockName: string): Promise<boolean>;
+}
+
+export class LockManager implements LockManagerInterface {
+  private static instance: LockManager | undefined;
+  private static prefix = 'SESSION_LOCK_';
+
+  constructor() {}
+
+  // 获取示例函数
+  static getInstance(): LockManagerInterface {
+    if (!LockManager.instance) {
+      const instance = new LockManager();
+      LockManager.instance = instance;
+      try {
+        if (!Reflect.has(window, 'MsgManager')) {
+          window.LockManager = instance;
+        }
+        if (!Reflect.has(window.self, 'MsgManager')) {
+          window.self.LockManager = instance;
+        }
+        if (!Reflect.has(window.top, 'MsgManager')) {
+          window.top.LockManager = instance;
+        }
+      } catch (error) {
+        window.LockManager = instance;
+      }
+    }
+    return LockManager.instance as LockManagerInterface;
+  }
+
+  async acquireLock(lockName: string): Promise<void> {
+    const lockKey = `${LockManager.prefix}${lockName}`;
+    const existingValue = await this.hasLock(lockName);
+    if (!existingValue) {
+      await window.sessionStorage.setItem(lockKey, 'true');
+    } else {
+      throw new Error('Lock already acquired.');
+    }
+  }
+
+  async releaseLock(lockName: string): Promise<void> {
+    const lockKey = `${LockManager.prefix}${lockName}`;
+    await window.sessionStorage.removeItem(lockKey);
+  }
+
+  async hasLock(lockName: string): Promise<boolean> {
+    const lockKey = `${LockManager.prefix}${lockName}`;
+    const value = await window.sessionStorage.getItem(lockKey);
+    return !!value;
+  }
+}
