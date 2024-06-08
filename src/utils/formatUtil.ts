66c1ce4895e8af7ee92d4398e95a60c996c3ba02
lwr <610507101@qq.com>
2024-06-08 14:41:33 +0800

    程序BUG：#16702金额格式化问题处理

--- src/utils/formatUtil.ts
+++ src/utils/formatUtil.ts
@@ -1,10 +1,13 @@
 export function formatNumber(value: string, format: string) {
   const reg1 = /.0+/; // 小数点.后0的个数表示保留几位
   const reg2 = /,#+/; // 逗号,后的#表示数字的分隔符为几位
-  let newValue = value;
+  let newValue = String(value);
   if (format?.match(reg1)?.[0]) {
     const fixedNum = format.match(reg1)[0].length - 1;
-    newValue = Number(value).toFixed(fixedNum);
+    let decimalPart = newValue.split('.')[1];
+    if (decimalPart?.length > fixedNum) {
+      newValue = Number(value).toFixed(fixedNum);
+    }
   } else {
     newValue = Number(value).toFixed(0);
   }
@@ -24,5 +27,7 @@ export function formatNumber(value: string, format: string) {
 
 export function formatMoney(value: string, format: string) {
   const prefix = format.slice(0, 1);
-  return prefix + formatNumber(value, format);
+  return value >= 0
+    ? prefix + formatNumber(value, format)
+    : '-' + prefix + formatNumber(value, format).slice(1);
 }
